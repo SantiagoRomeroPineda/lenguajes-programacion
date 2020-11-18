16e3c73689d593f39ba637f8fd0d571289ef5b08
SantiagoRomeroPineda <saropi9978@gmail.com>
2020-11-18 09:36:42 -0500

    arreglar 2do punto

--- taller_Scheme/taller.scm
+++ taller_Scheme/taller.scm
@@ -1,3 +1,4 @@
+;punto 1
 (define and-l (lambda (x) 
     (if (null? x)
         #t
@@ -23,10 +24,24 @@
 	(lambda (x) (* x x x))
 )
 
+
+;punto 2
+;incompleto, falta poner las listas
 (define valores(list -5 -1 0 10 15))
 
 (define evaluar-puntos
   (lambda(x y) (x y) )
 )
+(evaluar-puntos cuadrado 3)
 
-(define evaluar-puntos 3)
+
+;punto 3
+(define (es-creciente-en-punto f x)
+  (and (>=(f (+ x 1)) (f x)) (<=(f (- x 1)) (f x) ))
+)
+(es-creciente-en-punto id 0 )
+(es-creciente-en-punto id 1 )
+(es-creciente-en-punto neg 1 )
+(es-creciente-en-punto cuadrado -5 )
+
+;punto 4
