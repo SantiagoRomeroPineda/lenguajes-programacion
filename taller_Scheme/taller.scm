a92117fe724f94c0380dd8a982eb26b59e0b3f03
SantiagoRomeroPineda <saropi9978@gmail.com>
2020-11-18 12:07:16 -0500

    arreglar punto 2

--- taller_Scheme/taller.scm
+++ taller_Scheme/taller.scm
@@ -54,3 +54,31 @@
 (es-decreciente-en-punto cuadrado -5 )
 
 ;punto 5
+(define (es-creciente-en-muestra funcion lista)
+  (cond ((> (length lista) 1) 
+    (cond ((es-creciente-en-punto funcion (car lista)) 
+      (es-creciente-en-muestra funcion (cdr lista)))
+      (else #f )))
+  (else (es-creciente-en-punto funcion (car lista)))
+  )
+)
+(es-creciente-en-muestra id (list -5 -3 0 8) )
+(es-creciente-en-muestra neg (list -5 -3 0 8) )
+(es-creciente-en-muestra cuadrado (list -5 -3 0 8) )
+
+(es-creciente-en-muestra cuadrado (list 1 4 6) )
+
+;punto 6
+(define (es-decreciente-en-muestra funcion lista)
+  (cond ((> (length lista) 1) 
+    (cond ((es-decreciente-en-punto funcion (car lista)) 
+      (es-decreciente-en-muestra funcion (cdr lista)))
+      (else #f )))
+  (else (es-decreciente-en-punto funcion (car lista)))
+  )
+)
+
+(es-decreciente-en-muestra id (list -5 -3 0 8) )
+(es-decreciente-en-muestra neg (list -5 -3 0 8) )
+(es-decreciente-en-muestra cuadrado (list -5 -3 0 8) )
+(es-decreciente-en-muestra neg (list -5 -3 0 1) )
